bc4fa99f336d48aac3882a443adb05b736fd0711
unknown <meroo1995saggaf@outlook.com>
2023-11-26 12:47:03 +0300

    add list view to homepage

--- lib/auth/SignIn.dart
+++ lib/auth/SignIn.dart
@@ -1,128 +1,208 @@
-import 'package:coffee_app/auth/SignUp.dart';
+import 'package:coffee_app/auth/Signup.dart';
+import 'package:coffee_app/pages/HomePage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({super.key});
+  const SignIn({Key? key}) : super(key: key);
 
   @override
-  State<SignIn> createState() => _SignInState();
+  _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passController = TextEditingController();
+
+  void _handleSignUp() async {
+    try {
+      String email = _emailController.text;
+      String password = _passController.text;
+
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      print("User Registered: ${userCredential.user!.email}");
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      ); // Navigate to the home page
+    } catch (e) {
+      print("Error During Registration: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passController.dispose();
+    super.dispose();
+  }
+
+  bool passwordVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(179, 174, 169, 1),
-      body: Wrap(
-        children: [
-          Container(
-            child: Image.asset('assets/images/coffee_beans.png'),
-            padding: EdgeInsets.only(left: 100, top: 80),
-          ),
-          Container(
-            child: Text(
-              'Sign-In',
-              style: TextStyle(
-                  color: Color.fromRGBO(
-                    112,
-                    112,
-                    112,
-                    1,
-                  ),
-                  fontSize: 38,
-                  fontWeight: FontWeight.bold),
+      backgroundColor: Color.fromRGBO(237, 229, 218, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image.asset('assets/images/coffee_beans.png'),
+              width: 200,
+              margin: EdgeInsets.only(left: 5, top: 60),
             ),
-            padding: EdgeInsets.only(left: 40, top: 40),
-          ),
-          Container(
-            child: TextField(
-                decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(16),
-              prefixIcon: Icon(Icons.email),
-              hintText: 'Email',
-              filled: true,
-              fillColor: Color.fromRGBO(255, 210, 166, 1),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(style: BorderStyle.none),
-              ),
-            )),
-            padding: EdgeInsets.only(right: 40, top: 60, left: 40),
-          ),
-          Container(
-            child: TextField(
-                decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(16),
-              prefixIcon: Icon(Icons.visibility_off_outlined),
-              hintText: 'Password',
-              filled: true,
-              fillColor: Color.fromRGBO(255, 210, 166, 1),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(style: BorderStyle.none),
-              ),
-            )),
-            padding: EdgeInsets.only(right: 40, top: 18, left: 40),
-          ),
-          Container(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Sign-In',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromRGBO(255, 255, 255, 1),
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromRGBO(147, 122, 113, 1),
-                shadowColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                minimumSize: Size(300, 50),
-              ),
-            ),
-            padding: EdgeInsets.only(left: 50, top: 30),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 60, top: 40),
-                child: Text(
-                  'Do not have an account?  ',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Roboto',
-                    fontSize: 18,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 40),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUp()));
-                  },
-                  child: Text(
-                    'Sign-Up',
-                    style: TextStyle(
-                      color: Color.fromRGBO(147, 122, 113, 1),
-                      fontFamily: 'Roboto',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800,
+            SizedBox(height: 60),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.only(left: 4),
+                    child: Container(
+                      width: 340,
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          prefixIcon: Icon(Icons.email_rounded),
+                          hintText: 'Email',
+                          filled: true,
+                          fillColor: Color.fromRGBO(255, 210, 166, 1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide(style: BorderStyle.none),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.only(left: 4),
+                    child: Container(
+                      width: 340,
+                      child: TextFormField(
+                        obscureText: passwordVisible,
+                        controller: _passController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          prefixIcon: IconButton(
+                              icon: Icon(passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    passwordVisible = !passwordVisible;
+                                  },
+                                );
+                              }),
+                          alignLabelWithHint: false,
+                          filled: true,
+                          hintText: 'Password',
+                          fillColor: Color.fromRGBO(255, 210, 166, 1),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide(style: BorderStyle.none),
+                          ),
+                        ),
+                        keyboardType: TextInputType.visiblePassword,
+                        textInputAction: TextInputAction.done,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.only(left: 4, bottom: 2),
+                    child: Container(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _handleSignUp();
+                          }
+                        },
+                        child: Text(
+                          'Sign-In',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(147, 122, 113, 1),
+                          shadowColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          minimumSize: Size(300, 50),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 70, bottom: 1),
+                        child: Text(
+                          'Already have an account?  ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Roboto',
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(bottom: 2),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUp()),
+                            );
+                          },
+                          child: Text(
+                            'Sign-Up',
+                            style: TextStyle(
+                              color: Color.fromRGBO(147, 122, 113, 1),
+                              fontFamily: 'Roboto',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -10,19 +10,73 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(237, 229, 218, 1),
-      body: Wrap(
-        children: [
-          Scaffold(
-            backgroundColor: Color.fromRGBO(160, 41, 240, 1),
-            appBar: AppBar(
-              title: Text('home Page'),
+    return Stack(
+      children: [
+        Container(
+          color: Color.fromRGBO(237, 229, 218, 1),
+        ),
+        ListView(
+          padding: EdgeInsets.only(left: 20, bottom: 460),
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Coffee',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      decorationThickness: 2.85,
+                      color: Color.fromRGBO(54, 59, 75, 1)),
+                ),
+              ),
             ),
-            body: Container(child: Text('home page')),
-          ),
-        ],
-      ),
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Cup-Cake',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromRGBO(54, 59, 75, 1)),
+                ),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Cookies',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromRGBO(54, 59, 75, 1)),
+                ),
+              ),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Croissant',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromRGBO(54, 59, 75, 1)),
+                ),
+              ),
+            ),
+            Wrap(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 60),
+                  child: Image.asset('assets/images/search.png'),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 60),
+                  child: Image.asset('assets/images/shopping-bag.png'),
+                )
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
